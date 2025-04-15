4077a47330302424012eb7cc96d73acc8a0db1f3
wpplng <lina.wappling@gmail.com>
2025-04-15 10:59:25 +0200

    admin login

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
 
@@ -11,6 +13,10 @@ const PORT = 3000 || process.env.PORT;
 // connect to db
 connectDB();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(express.static('public'));
 
 // templating engine
@@ -19,6 +25,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
