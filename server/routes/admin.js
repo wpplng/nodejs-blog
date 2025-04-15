8a6962c8f3293e8244162d430469ae0cfd2022c2
wpplng <lina.wappling@gmail.com>
2025-04-15 11:38:51 +0200

    register user

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
 
@@ -35,6 +37,28 @@ router.post('/admin', async (req, res) => {
       res.send('You are logged in.');
     } else {
       res.send('Wrong username or password');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * POST /
+ * Admin - Register
+ */
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: 'User Created', user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: 'User already in use' });
+      }
+      resizeTo.status(500).json({ message: 'Internal server error' });
     }
   } catch (error) {
     console.log(error);
