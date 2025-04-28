d50d426fa12eabb0275a2742cb0d210e3f1ae8f9
wpplng <lina.wappling@gmail.com>
2025-04-28 11:54:19 +0200

    Show single post

--- server/routes/main.js
+++ server/routes/main.js
@@ -37,6 +37,31 @@ router.get('', async (req, res) => {
   }
 });
 
+/**
+ * GET /
+ * Post :id
+ */
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.',
+    };
+
+    res.render('post', {
+      locals,
+      data,
+      currentRoute: `/post/${slug}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get('/about', (req, res) => {
   res.render('about');
 });
