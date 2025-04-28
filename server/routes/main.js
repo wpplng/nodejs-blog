2c143b8bdd355e5d1584cf1bee86779d87a163d4
wpplng <lina.wappling@gmail.com>
2025-04-28 12:41:44 +0200

    Added search functionality

--- server/routes/main.js
+++ server/routes/main.js
@@ -62,6 +62,37 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
+/**
+ * POST /
+ * Post - searchTerm
+ */
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: 'Seach',
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.',
+    };
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, '');
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+      ],
+    });
+
+    res.render('search', {
+      data,
+      locals,
+      currentRoute: '/',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get('/about', (req, res) => {
   res.render('about');
 });
